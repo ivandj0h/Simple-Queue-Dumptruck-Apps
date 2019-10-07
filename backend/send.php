778879e9ba96fb45a94993a7e1f75f419b53156a
ivandi1980 <ivandi.djoh@gmail.com>
2019-10-07 11:18:10 +0800

    modif send

--- backend/send.php
+++ backend/send.php
@@ -1,15 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "password";
-$dbname = "hopper";
+// $servername = "localhost";
+// $username = "root";
+// $password = "password";
+// $dbname = "hopper";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+include 'db.php';
 
 $dat = $_GET["ultra"];
 
