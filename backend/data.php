09f051a8251095399fa5e752e0cdea280078220a
ivandi1980 <ivandi.djoh@gmail.com>
2019-10-07 16:33:35 +0800

    added alarm feature

--- backend/data.php
+++ backend/data.php
@@ -5,8 +5,8 @@ $r = $conn->query("SELECT * FROM antrian ORDER BY created_at DESC")->fetch_assoc
 $kode = $r['kode'];
 
 if($kode == '11') { echo "1"; }
-elseif($kode == 10) { echo "0"; }
-elseif($kode == 20) { echo "0"; }
+elseif($kode == 10) { echo "<span class=\"blink\">0</span>"; }
+elseif($kode == 20) { echo "<span class=\"blink\">0</span>"; }
 elseif($kode == 21) { echo "2"; }
-else { echo "<font color=red>x</font>"; }
+else { echo "<span class=\"blink\">X</span>"; }
 ?>
