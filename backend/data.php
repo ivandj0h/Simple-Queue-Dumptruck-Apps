d723b96f9a1f945f28643ce4f183d4c1c17a50f7
ivandi1980 <ivandi.djoh@gmail.com>
2019-10-07 15:10:22 +0800

    fix bugs unsyncron with time

--- backend/data.php
+++ backend/data.php
@@ -6,7 +6,7 @@ $kode = $r['kode'];
 
 if($kode == '11') { echo "1"; }
 elseif($kode == 10) { echo "0"; }
-elseif($kode == 20) { echo "2"; }
-elseif($kode == 21) { echo "0"; }
+elseif($kode == 20) { echo "0"; }
+elseif($kode == 21) { echo "2"; }
 else { echo "<font color=red>x</font>"; }
 ?>
--- backend/jam.php
+++ backend/jam.php
@@ -1,13 +1,10 @@
 <?php
 include 'db.php';
-
-$result = $conn->query("SELECT TIME(created_at) AS jam FROM antrian WHERE kode='11' ORDER BY created_at DESC")->fetch_assoc();
-$result2 = $conn->query("SELECT TIME(created_at) AS jam2, kode FROM antrian WHERE kode='10' ORDER BY created_at DESC")->fetch_assoc();
-
+$result = $conn->query("SELECT TIME(created_at) AS jam FROM antrian ORDER BY created_at DESC")->fetch_assoc();
+$result2 = $conn->query("SELECT TIME(created_at) AS jam2, kode FROM antrian ORDER BY created_at DESC")->fetch_assoc();
 $jam = $result['jam'];
 $jam2 = $result2['jam2'];
 $kode = $result2['kode'];
-
 if(Empty($jam)) { 
 	echo "<font color=red>No Data!</font>"; } 
 	else if($jam2 && $kode='10' || $jam2 && $kode='20'){ 
